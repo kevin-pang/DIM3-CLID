0f001ce62df00aa842babc2360837bbcd808765e
Kevin Pang <kbypang@gmail.com>
2014-03-17 17:00:03 +0800

    VER 1.01 - Model Setup
    Model and DB up

---
+++ auction/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    Username = models.CharField(max_length=128, unique=True)
+    Password = models.CharField(max_length=128)
+    Email = models.EmailField()
+    Telephone = models.CharField(max_length=10)
+    Address = models.CharField(max_length=128)
+    Type = models.CharField(max_length=10)
+    
+    def __unicode__(self):
+        return self.Username
+    
+class Product(models.Model):
+    ProductID = models.AutoField(primary_key=True)
+    ProductName = models.CharField(max_length=128, unique=True)
+    Description = models.CharField(max_length=128)
+    PostDate = models.DateTimeField()
+    SellingPrice = models.DecimalField(max_digits=8, decimal_places=2)
+    StartingBid =  models.DecimalField(max_digits=8, decimal_places=2)
+    EndDate = models.DateTimeField()
+    ViewNo = models.IntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.ProductName
+    
+class Bid(models.Model):
+    BidID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User)
+    ProductID = models.ForeignKey(Product)
+    BidNo = models.IntegerField(default=0)
+    Time = models.DateTimeField()
+    Quantity = models.IntegerField(default=0)
+    Subtotal =  models.DecimalField(max_digits=8, decimal_places=2)
+    
+    def __unicode__(self):
+        return self.BidID
+    
+class Comment(models.Model):
+    UserID = models.ForeignKey(User)
+    ProductID = models.ForeignKey(Product)
+    Comment = models.CharField(max_length=128)
+    
+class Rating(models.Model):
+    UserID = models.ForeignKey(User)
+    ProductID = models.ForeignKey(Product)
+    Rating = models.IntegerField(default=0)
